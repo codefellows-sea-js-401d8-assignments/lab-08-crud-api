f1330e64371857d62a94730bf786c8011d37f5ef
Gin <vienlyt@gmail.com>
2016-07-25 13:50:38 -0700

    put works with both url string query and json id selector

--- route/routes.js
+++ route/routes.js
@@ -6,14 +6,6 @@ const response = require('vien-simple-router').response;
 const movieList = {};
 
 module.exports = function(router) {
-  router.post('/movie', (req, res) => {
-    if(req.body && req.body.name && req.body.rating) {
-      let newMovie = new Movie(req.body.name, req.body.rating);
-      movieList[newMovie.id] = newMovie;
-      return response(200, newMovie)(res);
-    }
-    return response(400, 'bad request')(res);
-  });
 
   router.get('/movie', (req, res) => {
     if(req.url && req.url.query.id) {
@@ -23,12 +15,57 @@ module.exports = function(router) {
     return response(400, 'bad request')(res);
   });
 
+  router.post('/movie', (req, res) => {
+    if(req.body && req.body.name && req.body.rating) {
+      let newMovie = new Movie(req.body.name, req.body.rating);
+      movieList[newMovie.id] = newMovie;
+      return response(200, newMovie)(res);
+    }
+    return response(400, 'bad request')(res);
+  });
+
+  router.put('/movie', (req, res) => {
+    let movie;
+    if(req.url && req.url.query.id) {
+      movie = movieList[req.url.query.id];
+    } else if (req.body && req.body.id) {
+      movie = movieList[req.body.id];
+    }
+    if(movie && req.body) {
+      movie.name = req.body.name || movie.name;
+      movie.rating = req.body.rating || movie.rating;
+      return response(200, movie)(res);
+    }
+
+    return response(400, 'bad request')(res);
+
+    // if(req.body && req.body.id) {
+    //   let movie = movieList[req.body.id];
+    //   if(movie) {
+    //     movie.name = req.body.name || movie.name;
+    //     movie.rating = req.body.rating || movie.rating;
+    //     return response(200, movie)(res);
+    //   }
+    //   return response(400, 'bad request')(res);
+    // }
+  });
+
+  router.delete('/movie', (req, res) => {
+    if(req.url && req.url.query.id) {
+      let movie = movieList[req.url.query.id];
+      if(movie) {
+        delete movieList[req.url.query.id];
+        return response(204, null)(res);
+      }
+      return response(400, 'bad request')(res);
+    }
+  });
+
   router.get('/movie/all', (req, res) => {
     console.log(movieList);
     let allMovies = Object.keys(movieList).map((id) => {
       return movieList[id];
     });
-
     return response(200, allMovies)(res);
   });
 };
