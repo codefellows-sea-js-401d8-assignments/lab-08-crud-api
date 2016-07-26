033560b24ff7f74560d7c43ff393a4645c22398a
Gin <vienlyt@gmail.com>
2016-07-25 21:46:16 -0700

    some necessary returns

--- route/routes.js
+++ route/routes.js
@@ -6,21 +6,22 @@ const response = require('vien-simple-router').response;
 const movieList = {};
 
 module.exports = function(router) {
+
   router.get('/movie', (req, res) => {
     if(req.url && req.url.query.id) {
       let movie = movieList[req.url.query.id];
-      response(200, movie)(res);
+      return response(200, movie)(res);
     }
-    response(400, 'bad request')(res);
+    return response(400, 'bad request')(res);
   });
 
   router.post('/movie', (req, res) => {
     if(req.body && req.body.name && req.body.rating) {
       let newMovie = new Movie(req.body.name, req.body.rating);
       movieList[newMovie.id] = newMovie;
-      response(200, newMovie)(res);
+      return response(200, newMovie)(res);
     }
-    response(400, 'bad request')(res);
+    return response(400, 'bad request')(res);
   });
 
   router.put('/movie', (req, res) => {
@@ -33,9 +34,9 @@ module.exports = function(router) {
     if(movie && req.body) {
       movie.name = req.body.name || movie.name;
       movie.rating = req.body.rating || movie.rating;
-      response(200, movie)(res);
+      return response(200, movie)(res);
     }
-    response(400, 'bad request')(res);
+    return response(400, 'bad request')(res);
   });
 
   router.delete('/movie', (req, res) => {
@@ -43,16 +44,17 @@ module.exports = function(router) {
       let movie = movieList[req.url.query.id];
       if(movie) {
         delete movieList[req.url.query.id];
-        response(204, null)(res);
+        return response(204, null)(res);
       }
-      response(400, 'bad request')(res);
+      return response(400, 'bad request')(res);
     }
   });
 
   router.get('/movie/all', (req, res) => {
+    console.log(movieList);
     let allMovies = Object.keys(movieList).map((id) => {
       return movieList[id];
     });
-    response(200, allMovies)(res);
+    return response(200, allMovies)(res);
   });
 };
