1d5399af9f02e8682130cff0f7767a428ef0b983
keiran beaton <keiran beaton>
2016-09-07 22:30:27 -0700

    added gulpfile, still need tests

---
+++ lab-keiran-beaton/gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const eslint = require('gulp-eslint');
+const nodemon = require('gulp-nodemon');
+
+let testFiles = ['./test/*.js'];
+let scriptFiles = ['./lib/*.js', './model/*.js'];
+
+gulp.task('lint', () => {
+  return gulp.src([scriptFiles, testFiles])
+  .pipe(eslint());
+});
+
+gulp.task('test', () => {
+  return gulp.src(testFiles)
+  .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('start', () => {
+  nodemon({script: 'server.js', ext: 'js html', env: {'NODE_ENV':'development'}});
+});
+
+gulp.task('watch', () => {
+  gulp.watch([scriptFiles, testFiles], ['lint', 'test']);
+});
+
+gulp.task('default', ['lint', 'test']);
